3274426bd05ea0a42ac9f4531253a1e11ca7e2ff
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-04-16 13:46:46 -0500

    started file structure, client sources and server routes

---
+++ src/js/install.js
@@ -0,0 +1,8 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+window.addEventListener("beforeinstallprompt", (event) => {});
+
+butInstall.addEventListener("click", async () => {});
+
+window.addEventListener("appinstalled", (event) => {});
