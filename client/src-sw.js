95a69c2592f43a90ea8da263d92d732cdcecd650
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-04-16 19:58:05 -0500

    added photos and fabicon

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -26,4 +26,16 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-registerRoute();
+// TODO: Implement asset caching
+// Set up asset cache
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,24 @@
 const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-window.addEventListener("beforeinstallprompt", (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
+});
 
-butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
 
-window.addEventListener("appinstalled", (event) => {});
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
